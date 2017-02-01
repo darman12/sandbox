e68f33af96a974fb5f8c38baf022385cc9ddc3ae
Spencer <spencer.freebairn@gmail.com>
2017-02-01 11:40:20 -0800

    added code to verify selected file

--- sandbox.cpp
+++ sandbox.cpp
@@ -5,6 +5,19 @@ using namespace std;
 
 char fileName[256];
 
+// checks to see if the file can be written to
+bool checkFile()
+{
+    ofstream fout(fileName);
+    if (fout.fail()) {
+        return false;
+    }
+
+    fout.close();
+    return true;
+}
+
+// writes user-inputted name to specified file
 void writeName(string name)
 {
     ofstream fout(fileName);          // declares stream variable and
@@ -14,6 +27,7 @@ void writeName(string name)
     fout.close();
 }
 
+// logs number of names entered to log.txt
 void logNumNames(float numNames)
 {
     ofstream fout;
@@ -22,10 +36,11 @@ void logNumNames(float numNames)
     fout.close();
 }
 
+// gets names from user, sends to writeName()
 float getNames()
 {
     string name;
-    float numNames;
+    float numNames = 0;
     string ans = "y";
 
     while (ans != "n")
@@ -51,6 +66,11 @@ int main()
     cout << "What is the name of the file you wish to store the names in? ";
     cin >> fileName;
 
+    while (checkFile() == false) {
+        cout << "Cannot write to that file, please select another: " << endl;
+        cin >> fileName;
+    }
+    cout << "Names will be stored in \"" << fileName << "\".\n";
     float numNames = getNames();
     logNumNames(numNames);
 

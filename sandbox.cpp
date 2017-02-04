5ae0eda11588dba373932406ad5f7c69b6def061
Spencer <spencer.freebairn@gmail.com>
2017-02-03 23:17:26 -0800

    reads log.txt to report how many names saved
    - doesn't yet read correct log entry, always reads first number
    - wanted to commit changes to at least have the progress up here

--- sandbox.cpp
+++ sandbox.cpp
@@ -5,45 +5,72 @@ using namespace std;
 
 char fileName[256];
 
+// checks to see if the file can be written to
+bool checkFile()
+{
+    ofstream fout(fileName, ios::app);
+    if (fout.fail()) {
+        return false;
+    }
+
+    fout.close();
+    return true;
+}
+
+// writes user-inputted name to specified file
 void writeName(string name)
 {
-    ofstream fout(fileName);          // declares stream variable and
-                                      //    associates it with a file
+    ofstream fout(fileName, ios::app);          // declares stream variable and
+                                                //    associates it with a file
     fout << name << endl;
 
     fout.close();
 }
 
+// logs number of names entered to log.txt
 void logNumNames(float numNames)
 {
     ofstream fout;
-    fout.open("log.txt");
-    fout << numNames;
+    fout.open("log.txt", ios::app);
+    fout << numNames << "\n";
     fout.close();
 }
 
+// gets names from user, sends to writeName()
 float getNames()
 {
     string name;
     float numNames;
     string ans = "y";
 
-    while (ans != "n")
+    for (numNames = 0; ans != "n"; numNames++) {
         if (ans == "y" || ans == "yes") {
             cout << "Enter a name: ";
             cin >> name;
             writeName(name);
             cout << "Do you want to enter another name? (y/n) ";
             cin >> ans;
-            numNames++;
         }
         else {
             cout << "Please enter a valid answer: ";
             cin >> ans;
         }
-    cout << "Thank you, names have been saved." << endl;
+    }
 
     return numNames;
+}
+
+void readLog(string fileName)
+{
+    ifstream fin("log.txt");
+
+    if (!fin.fail()) {
+        int value = 0;
+        fin >> value;
+        cout << value << " names were saved to \"" << fileName << "\"." << endl;
+    }
+
+    fin.close();
 }
 
 int main()
@@ -51,8 +78,13 @@ int main()
     cout << "What is the name of the file you wish to store the names in? ";
     cin >> fileName;
 
-    float numNames = getNames();
-    logNumNames(numNames);
+    while (checkFile() == false) {
+        cout << "Cannot write to that file, please select another: " << endl;
+        cin >> fileName;
+    }
+    cout << "Names will be stored in \"" << fileName << "\".\n";
+    logNumNames(getNames());
+    readLog(fileName);
 
     return 0;
 }

7e866b1f4ff558c607a826e0a3ba335d336d52ba
Spencer <spencer.freebairn@gmail.com>
2017-02-03 23:22:45 -0800

    Add files via upload

--- sandbox.cpp
+++ sandbox.cpp
@@ -8,7 +8,7 @@ char fileName[256];
 // checks to see if the file can be written to
 bool checkFile()
 {
-    ofstream fout(fileName);
+    ofstream fout(fileName, ios::app);
     if (fout.fail()) {
         return false;
     }
@@ -20,8 +20,8 @@ bool checkFile()
 // writes user-inputted name to specified file
 void writeName(string name)
 {
-    ofstream fout(fileName);          // declares stream variable and
-                                      //    associates it with a file
+    ofstream fout(fileName, ios::app);          // declares stream variable and
+                                                //    associates it with a file
     fout << name << endl;
 
     fout.close();
@@ -31,8 +31,8 @@ void writeName(string name)
 void logNumNames(float numNames)
 {
     ofstream fout;
-    fout.open("log.txt");
-    fout << numNames;
+    fout.open("log.txt", ios::app);
+    fout << numNames << "\n";
     fout.close();
 }
 
@@ -40,25 +40,37 @@ void logNumNames(float numNames)
 float getNames()
 {
     string name;
-    float numNames = 0;
+    float numNames;
     string ans = "y";
 
-    while (ans != "n")
+    for (numNames = 0; ans != "n"; numNames++) {
         if (ans == "y" || ans == "yes") {
             cout << "Enter a name: ";
             cin >> name;
             writeName(name);
             cout << "Do you want to enter another name? (y/n) ";
             cin >> ans;
-            numNames++;
         }
         else {
             cout << "Please enter a valid answer: ";
             cin >> ans;
         }
-    cout << "Thank you, names have been saved." << endl;
+    }
 
     return numNames;
+}
+
+void readLog(string fileName)
+{
+    ifstream fin("log.txt");
+
+    if (!fin.fail()) {
+        int value = 0;
+        fin >> value;
+        cout << value << " names were saved to \"" << fileName << "\"." << endl;
+    }
+
+    fin.close();
 }
 
 int main()
@@ -71,8 +83,8 @@ int main()
         cin >> fileName;
     }
     cout << "Names will be stored in \"" << fileName << "\".\n";
-    float numNames = getNames();
-    logNumNames(numNames);
+    logNumNames(getNames());
+    readLog(fileName);
 
     return 0;
 }

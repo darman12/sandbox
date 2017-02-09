818f6871d796bb93bb34c91e540122969ea690ee
Spencer <spencer.freebairn@gmail.com>
2017-02-08 18:44:53 -0800

    currently overhauling file-checking

--- sandbox.cpp
+++ sandbox.cpp
@@ -3,22 +3,23 @@
 #include <iomanip>
 using namespace std;
 
-char fileName[256];
-
-// checks to see if the file can be written to
-bool checkFile()
+bool getFileName (char fileName[])
 {
+    cout << "Enter the name of the file you wish to store the names in: ";
+    cin >> fileName;
+
     ofstream fout(fileName, ios::app);
     if (fout.fail()) {
+        cout << "Unable to save to \"" << fileName << "\"." << endl;
         return false;
     }
-
-    fout.close();
-    return true;
+    else {
+        return true;
+    }
 }
 
 // writes user-inputted name to specified file
-void writeName(string name)
+void writeName(string name, char fileName[])
 {
     ofstream fout(fileName, ios::app);          // declares stream variable and
                                                 //    associates it with a file
@@ -26,7 +27,7 @@ void writeName(string name)
 
     fout.close();
 }
-
+/*
 // logs number of names entered to log.txt
 void logNumNames(float numNames)
 {
@@ -35,9 +36,9 @@ void logNumNames(float numNames)
     fout << numNames << "\n";
     fout.close();
 }
-
+*/
 // gets names from user, sends to writeName()
-float getNames()
+float getNames(char fileName[])
 {
     string name;
     float numNames;
@@ -47,7 +48,7 @@ float getNames()
         if (ans == "y" || ans == "yes") {
             cout << "Enter a name: ";
             cin >> name;
-            writeName(name);
+            writeName(name, fileName);
             cout << "Do you want to enter another name? (y/n) ";
             cin >> ans;
         }
@@ -60,31 +61,38 @@ float getNames()
     return numNames;
 }
 
-void readLog(string fileName)
+/*
+void readLog(char fileName[])
 {
     ifstream fin("log.txt");
-
     if (!fin.fail()) {
         int value = 0;
         fin >> value;
         cout << value << " names were saved to \"" << fileName << "\"." << endl;
     }
 
+    cout << "The following names are stored in \"" << fileName << "\"." << endl;
+
+    ifstream finNames("names.txt");
+    while (!finNames.eof()) {
+        char name[256];
+        finNames >> name;
+        cout << name << endl;
+    }
     fin.close();
 }
+*/
 
 int main()
 {
-    cout << "What is the name of the file you wish to store the names in? ";
-    cin >> fileName;
+    char fileName[256];
+    //float numNames = 0;
 
-    while (checkFile() == false) {
-        cout << "Cannot write to that file, please select another: " << endl;
-        cin >> fileName;
-    }
-    cout << "Names will be stored in \"" << fileName << "\".\n";
-    logNumNames(getNames());
-    readLog(fileName);
+
+    getFileName(fileName);
+    //numNames = getNames(fileName);
+    //logNumNames(getNames());
+    //readLog(fileName);
 
     return 0;
 }
